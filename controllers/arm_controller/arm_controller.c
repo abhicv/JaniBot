8f297d7fb487f45477b215b51feabb41d7b26690
abhicv <abhijithcheruvery@gmail.com>
2021-03-10 20:18:21 +0530

    added robot blender file

--- controllers/arm_controller/arm_controller.c
+++ controllers/arm_controller/arm_controller.c
@@ -1,6 +1,7 @@
 #include <webots/robot.h>
 #include <webots/keyboard.h>
 #include <webots/motor.h>
+
 #include <stdio.h>
 
 #define TIME_STEP 64
@@ -19,39 +20,39 @@ int main(int argc, char **argv)
   wb_motor_set_velocity(motor1, 0.0);   
   wb_motor_set_velocity(motor2, 0.0);
   
-  double leftSpeed = 0.0f;
-  double rightSpeed = 0.0f;
+  double yawSpeed = 0.0f;
+  double pitchSpeed = 0.0f;
   
   wb_keyboard_enable(16);
   
   while (wb_robot_step(TIME_STEP) != -1) 
   {
-      leftSpeed = 0.0f;
-      rightSpeed = 0.0f;
+      yawSpeed = 0.0f;
+      pitchSpeed = 0.0f;
       
       char c = wb_keyboard_get_key();
       
       switch(c)
       {
           case 'W':
-          leftSpeed = -1.0f;
+          yawSpeed = 1.0f;
           break;
           
           case 'S':
-          leftSpeed = 1.0f;
+          yawSpeed = -1.0f;
           break;
           
           case 'A':
-          rightSpeed = -1.0f;
+          pitchSpeed = 1.0f;
           break;
           
           case 'D':
-          rightSpeed = -1.0f;
+          pitchSpeed = -1.0f;
           break;
       }   
       
-      wb_motor_set_velocity(motor1, SPEED * rightSpeed);
-      wb_motor_set_velocity(motor2, SPEED * leftSpeed);
+      wb_motor_set_velocity(motor1, SPEED * pitchSpeed);
+      wb_motor_set_velocity(motor2, SPEED * yawSpeed);
   };
 
   wb_robot_cleanup();
