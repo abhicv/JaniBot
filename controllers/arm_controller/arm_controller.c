95a6c24f5df88dc7fcda1b99921c88618b052e10
abhicv <abhijithcheruvery@gmail.com>
2021-03-12 00:07:57 +0530

    3d models added

--- controllers/arm_controller/arm_controller.c
+++ controllers/arm_controller/arm_controller.c
@@ -11,48 +11,26 @@ int main(int argc, char **argv)
 {
   wb_robot_init();
   
-  WbDeviceTag motor1 = wb_robot_get_device("motor1");
-  WbDeviceTag motor2 = wb_robot_get_device("motor2");
+  WbDeviceTag baseMotor = wb_robot_get_device("base_motor");
   
-  wb_motor_set_position(motor1, INFINITY);
-  wb_motor_set_position(motor2, INFINITY);
-  
-  wb_motor_set_velocity(motor1, 0.0);   
-  wb_motor_set_velocity(motor2, 0.0);
-  
-  double yawSpeed = 0.0f;
   double pitchSpeed = 0.0f;
   
   wb_keyboard_enable(16);
   
   while (wb_robot_step(TIME_STEP) != -1) 
   {
-      yawSpeed = 0.0f;
       pitchSpeed = 0.0f;
       
       char c = wb_keyboard_get_key();
       
       switch(c)
       {
-          case 'W':
-          yawSpeed = 1.0f;
-          break;
-          
-          case 'S':
-          yawSpeed = -1.0f;
-          break;
-          
           case 'A':
           pitchSpeed = 1.0f;
           break;
-          
-          case 'D':
-          pitchSpeed = -1.0f;
-          break;
       }   
       
-      wb_motor_set_velocity(motor1, SPEED * pitchSpeed);
-      wb_motor_set_velocity(motor2, SPEED * yawSpeed);
+      wb_motor_set_velocity(baseMotor, SPEED * pitchSpeed);
   };
 
   wb_robot_cleanup();
