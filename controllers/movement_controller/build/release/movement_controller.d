8d0412c343c425c8c7a0b83a009342113ab780c1
abhicv <abhijithcheruvery@gmail.com>
2021-05-18 09:43:33 +0530

    fusion 360 urdf export

--- controllers/movement_controller/build/release/movement_controller.d
+++
@@ -1,6 +0,0 @@
-build/release/movement_controller.o: movement_controller.c \
- E:\Webots/include/controller/c/webots/robot.h \
- E:\Webots/include/controller/c/webots/types.h \
- E:\Webots/include/controller/c/webots/nodes.h \
- E:\Webots/include/controller/c/webots/motor.h \
- E:\Webots/include/controller/c/webots/keyboard.h
