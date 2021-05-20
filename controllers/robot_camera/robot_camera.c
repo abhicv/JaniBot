4ea37a49a6b5248860a2c58623a9d5a2fc7c2ca6
abhicv <abhijithcheruvery@gmail.com>
2021-05-20 18:59:56 +0530

    added real sense model

---
+++ controllers/robot_camera/robot_camera.c
@@ -0,0 +1,20 @@
+#include <webots/robot.h>
+#include <webots/camera.h>
+
+#define TIME_STEP 64
+
+int main(int argc, char **argv) 
+{
+	wb_robot_init();
+	WbDeviceTag camera = wb_robot_get_device("camera");
+
+	wb_camera_enable(camera, TIME_STEP);
+	while (wb_robot_step(TIME_STEP) != -1) 
+	{
+	
+	};
+	
+	wb_robot_cleanup();
+	
+	return 0;
+}
