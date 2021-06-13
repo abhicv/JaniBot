b97c67cae71a91345fd795c392e8d0d306d67ff4
ai-zer <52460321+ai-zer@users.noreply.github.com>
2021-06-13 10:59:46 +0530

    Delete controllers directory

--- controllers/arm_controller/build/release/arm_controller.d
+++
@@ -1,6 +0,0 @@
-build/release/arm_controller.o: arm_controller.c \
- E:\Webots/include/controller/c/webots/robot.h \
- E:\Webots/include/controller/c/webots/types.h \
- E:\Webots/include/controller/c/webots/nodes.h \
- E:\Webots/include/controller/c/webots/keyboard.h \
- E:\Webots/include/controller/c/webots/motor.h
